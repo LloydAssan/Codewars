9aa0befb3104311a343e0adc12dc28d709c3c761
Lloyd Assan <55921640+LloydAssan@users.noreply.github.com>
2019-10-23 09:12:39 -0400

    Add files via upload

---
+++ drinkAbout.js
@@ -0,0 +1,14 @@
+
+// Drink about
+
+function peopleWithAgeDrink(old) {
+  if(old <= 13){
+  return "drink toddy"
+  }else if(old >= 14 && old <= 17){
+  return "drink coke"
+  }else if(old >= 18 && old <= 20){
+  return "drink beer"
+  }else {
+  return "drink whisky"
+  }
+};
