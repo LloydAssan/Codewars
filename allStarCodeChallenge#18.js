32867b86b1f7e7d870c6b3091605602aa2e2d464
Lloyd Assan <55921640+LloydAssan@users.noreply.github.com>
2019-12-28 00:35:02 -0500

    Add files via upload

---
+++ allStarCodeChallenge#18.js
@@ -0,0 +1,12 @@
+// This Kata is intended as a small challenge for my students
+//
+// All Star Code Challenge #18
+//
+// Create a function called that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument is found in the first one.
+// 
+// If no occurrences can be found, a count of 0 should be returned.
+
+
+function strCount(str, letter){
+  return str.split(letter).length - 1;
+}
